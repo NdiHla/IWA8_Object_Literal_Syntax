c680c8c0dd38017ea399c13557aa82089a557fc1
Ndimande <mkatroso.deep@gmail.com>
2023-04-15 23:23:46 +0200

    Syntax

---
+++ Leo scrpts.js
@@ -0,0 +1,11 @@
+const leo{
+    (leoName='leoName + leoSurname')
+   (balance = 'leoBalance')
+   (accessId = '47afb389-8014-4d0b-aff3-e40203d2107f')
+   (age ='24')
+   (address
+       (number = 'leoNumber')
+       (street = 'leoStreet')
+       (postalCode = 'leoPostal')
+   )
+}
