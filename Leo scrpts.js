880ad9b8808556f5c52081293fe0bb3ac7aefc4c
Ndimande <mkatroso.deep@gmail.com>
2023-04-24 21:38:03 +0200

    IWA8

--- Leo scrpts.js
+++
@@ -1,9 +0,0 @@
-const {
-	LeoName = 'leoName'
-	(accessId = '47afb389-8014-4d0b-aff3-e40203d2107f')
-	balance = 'leoBalance' 
-	(age = 24)
-	 	(number = 'leoNumber')
-		(street = 'leoStreet')
-		(postalCode =' leoPostal')	
-}
